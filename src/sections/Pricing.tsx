4caca6be2d5f9725a5646638c54be7a53fe3262b
Cem Bıçakcı <cmbicakci@gmail.com>
2024-09-01 13:51:39 +0300

    pricing section

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,3 +1,55 @@
+import clsx from 'clsx';
+import CheckIcon from '@/assets/check.svg'
+
+export const Pricing = () => {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <div className='section-heading'>
+          <h2 className="section-title">Pricing</h2>
+          <p className="section-description mt-5">
+            Free forever. Upgrade for unlimited tasks, better security, and exclusive features.
+          </p>
+        </div>
+        <div className='flex flex-col lg:flex-row lg:items-end lg:justify-center gap-6 items-center mt-10'>
+          {
+            pricingTiers.map(({ title, monthlyPrice, buttonText, popular, inverse, features }, index) => (
+              <div key={index} className={clsx('p-10 border border-[#F1F1F1] rounded-3xl shadow-[0_7px_14px_#EAEAEA] max-w-xs w-full ', inverse && 'border-black bg-black text-white')}>
+                <div className='flex justify-between'>
+                  <h3 className={clsx('text-lg font-bold text-black/50', inverse && 'text-white/70')}>{title}</h3>
+                  {
+                    popular && (
+                      <div className='inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20'>
+                        <span className='bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] text-transparent bg-clip-text font-medium'>Popular</span>
+                      </div>
+                    )
+                  }
+                </div>
+                <div className='flex items-baseline gap-1 mt-[30px]'>
+                  <span className='text-4xl font-bold tracking-tighter leading-none'>${monthlyPrice}</span>
+                  <span className='tracking-tight font-bold text-black/50'>/month</span>
+                </div>
+                <button className={clsx('btn btn-primary w-full mt-[30px]', inverse && 'bg-white text-black')}>{buttonText}</button>
+                <ul className='flex flex-col gap-5 mt-8'>
+                  {
+                    features.map((feature, index) => (
+                      <li key={index} className='text-sm flex items-center gap-4'>
+                        <CheckIcon className="h-6 w-6" />
+                        <span>{feature}</span>
+                      </li>
+                    ))
+                  }
+                </ul>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </section>
+  )
+};
+
+
 const pricingTiers = [
   {
     title: "Free",
@@ -49,7 +101,3 @@ const pricingTiers = [
     ],
   },
 ];
-
-export const Pricing = () => {
-  return null;
-};
